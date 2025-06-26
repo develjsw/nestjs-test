4196771d4cca181e5f72f1c5fc2fae56dd7322d1
jsw <develjsw@gmail.com>
2025-06-26 23:25:41 +0900

    [payment] payment 모듈 초기화

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './common/prisma/prisma.module';
 import { CommunityModule } from './module/community/community.module';
+import { PaymentModule } from './module/payment/payment.module';
 
 @Module({
-    imports: [PrismaModule, CommunityModule],
+    imports: [PrismaModule, CommunityModule, PaymentModule],
     controllers: [AppController],
     providers: [AppService],
 })
