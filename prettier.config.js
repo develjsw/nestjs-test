a20f418039d1dbcab533bf85e493a10fd25f86de
jsw <develjsw@gmail.com>
2025-06-26 20:32:27 +0900

    [init] nestjs 초기화

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  singleQuote: true,
+  trailingComma: 'all',
+  semi: true,
+  printWidth: 120,
+  tabWidth: 4,
+  useTabs: false,
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+};
