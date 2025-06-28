b312322315f4c8dc43fdd8e3183969308650e1da
jsw <develjsw@gmail.com>
2025-06-29 01:46:25 +0900

    [payment] 미사용 모듈 제거

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './common/prisma/prisma.module';
 import { CommunityModule } from './module/community/community.module';
-import { PaymentModule } from './module/payment/payment.module';
 import { ShareModule } from './module/share/share.module';
 import { UserModule } from './module/user/user.module';
 
 @Module({
-    imports: [PrismaModule, CommunityModule, PaymentModule, ShareModule, UserModule],
+    imports: [PrismaModule, CommunityModule, ShareModule, UserModule],
     controllers: [AppController],
     providers: [AppService],
 })
